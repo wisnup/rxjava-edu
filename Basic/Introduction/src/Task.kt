58f339963ced1df0092c8539b49bc74e66efd153
Wisnu Priyambodo <wisnu.priyambodo@quipper.com>
2021-04-01 17:47:18 +0700

    Update comment

--- Basic/Introduction/src/Task.kt
+++ Basic/Introduction/src/Task.kt
@@ -8,8 +8,8 @@ object Introduction {
     }
 
     private fun createAndSubscribeObservable() {
-        Observable.just(1, 2, 3) // create an observable
-                .subscribe { // subscribe; observable won't do anything if no one subscribed to it
+        Observable.just(1, 2, 3) // observable
+                .subscribe { // subscriber/observer; observable won't do anything if no one subscribed to it
                     println("$it") // observable is just a stream of data, it doesn't do anything unless you do something with the emitted data.
                 }
         // okay, operator is optional but where's the scheduler tho?
